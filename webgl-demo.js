7da3898116e9eacf80982e596e44f38fc561e9a6
Kunal Garg <kunalgarg2100@gmail.com>
2018-03-28 22:27:22 +0530

    Add camera

--- webgl-demo.js
+++ webgl-demo.js
@@ -1018,10 +1018,24 @@ function handleKeys(shapes){
                 shapes[i].rotation_Z -= shapes[i].rotation;
             }
     }
-    
+    if(statusKeys[65]){
+        console.log('right cameraAngleRadians')
+        updateCameraAngle();
+    }
 }
 
+function radToDeg(r) {
+    return r * 180 / Math.PI;
+  }
 
+  function degToRad(d) {
+    return d * Math.PI / 180;
+  }
+
+  var cameraAngleRadians = 0;
+  function updateCameraAngle() {
+    cameraAngleRadians++;
+  }
 //  var fieldOfViewRadians = degToRad(60);
 
 function clearScene(gl){
@@ -1055,6 +1069,37 @@ function clearScene(gl){
                      zNear,
                      zFar);
 
+    var numFs = 5;
+    var radius = 1;
+     
+    var fPosition = [0, 0, 0];
+ 
+  // Use matrix math to compute a position on a circle where
+  // the camera is
+  //var cameraMatrix = m4.yRotation(degToRad());
+  const cameraMatrix = mat4.create();
+  mat4.rotate(cameraMatrix,  // destination matrix
+                cameraMatrix,  // matrix to rotate
+                degToRad(cameraAngleRadians),     // amount to rotate in radians
+                [0, 1, 0]);
+  mat4.translate(cameraMatrix, cameraMatrix, [0, 0, radius * 1.5]);
+ 
+  // Get the camera's postion from the matrix we computed
+  var cameraPosition = [
+    cameraMatrix[12],
+    cameraMatrix[13],
+    cameraMatrix[14],
+  ];
+ 
+  var up = [0, 1, 0];
+ 
+  // Compute the camera's matrix using look at.
+  var cameraMatrix1 = lookAt(cameraPosition, fPosition, up);
+ 
+  // Make a view matrix from the camera matrix.
+  mat4.invert(cameraMatrix1, cameraMatrix1);
+        // Compute a view projection matrix
+  mat4.multiply(projectionMatrix, projectionMatrix,cameraMatrix1);
   return projectionMatrix;
 }
 
@@ -1299,3 +1344,38 @@ function loadShader(gl, type, source) {
 
 
 main();
+function lookAt(cameraPosition, target, up) {
+    var zAxis = normalize(
+        subtractVectors(cameraPosition, target));
+    var xAxis = cross(up, zAxis);
+    var yAxis = cross(zAxis, xAxis);
+
+    return [
+       xAxis[0], xAxis[1], xAxis[2], 0,
+       yAxis[0], yAxis[1], yAxis[2], 0,
+       zAxis[0], zAxis[1], zAxis[2], 0,
+       cameraPosition[0],
+       cameraPosition[1],
+       cameraPosition[2],
+       1,
+    ];
+    };
+function subtractVectors(a, b) {
+  return [a[0] - b[0], a[1] - b[1], a[2] - b[2]];
+}
+
+function normalize(v) {
+  var length = Math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+  // make sure we don't divide by 0.
+  if (length > 0.00001) {
+    return [v[0] / length, v[1] / length, v[2] / length];
+  } else {
+    return [0, 0, 0];
+  }
+}
+
+function cross(a, b) {
+  return [a[1] * b[2] - a[2] * b[1],
+          a[2] * b[0] - a[0] * b[2],
+          a[0] * b[1] - a[1] * b[0]];
+}
